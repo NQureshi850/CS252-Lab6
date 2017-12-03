72615a427f17c846e55a20d1257270dd9de224e7
Navillus <rps214@verizon.net>
2017-12-03 18:08:17 -0500

    Add user model with score api call

---
+++ model/User.js
@@ -0,0 +1,33 @@
+
+export class User {
+	constructor(username) {
+		this.score = 0;
+		this.username = username;
+		this.getUserData();
+
+
+	}
+
+	getUserData() {
+		fetch("http://data.cs.purdue.edu/PLACEHOLDER") {
+			method: "POST",
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"	
+			},
+			body: JSON.stringify({
+				username: this.username;	
+			})
+		}).then(function(response) {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error("Network response was not ok");
+		}).then(function(data) {
+			for (let prop in data) {
+				this.score = user["score"];
+					
+			}
+		});
+	}
+}
