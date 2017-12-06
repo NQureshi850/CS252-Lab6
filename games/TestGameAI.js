7850ba75f917407f70ba6776d332c60c481c3282
Navillus <ryanpnavillus@gmail.com>
2017-12-06 02:52:27 -0500

    Rename files

---
+++ games/TestGameAI.js
@@ -0,0 +1,53 @@
+class TestGameAI extends Game {
+	constructor() {
+		super("TestGameAI");		
+		this.scorePenalty = 100; 
+		this.player1 = this.addUser();
+		this.ai = this.addAI();
+	}
+
+	start() {
+		super.start();
+		this.runGame();
+	}
+
+	runGame() {
+		var shouldContinue = true;
+		while(shouldContinue) 
+		{
+			var playerAction = this.requestTurn(this.player1);
+			var aiAction = this.requestTurn(this.ai);
+			if ( ((playerAction + aiAction) % 3) == 0 ) 
+			{
+				console.log(this.player1.username + " wins");
+				this.increasePlayerScore(this.player1, this.scorePenalty);
+				this.end(this.player1);
+				shouldContinue = false;
+			}
+			else if ( ((playerAction + aiAction ) % 3) == 1)
+			{
+				console.log(this.ai.username + " wins");
+				this.decreasePlayerScore(this.player1, this.scorePenalty);
+				this.end(this.ai);
+				shouldContinue = false;
+			}
+		}
+	}
+
+	requestTurn(player) {
+		if(player instanceof AI) 
+		{
+			// player is an AI
+			return player.pickRandomOption(3);
+		}
+		else
+		{
+			// player is human
+			var number = prompt(player.username);
+			return number
+		}
+	}
+}
+
+var game = new TestGameAI();
+game.start();
