617fb8e79a3d41f046df4434de0d9fdfc6f8bf72
Ryan Sullivan <ryanpnavillus@gmail.com>
2017-12-07 02:09:01 -0500

    Add ScoreBoard.js

---
+++ model/ScoreBoard.js
@@ -0,0 +1,29 @@
+class ScoreBoard {
+	constructor(gameName) {
+		this.game = gameName;
+
+		//Map of {player : score}
+		this.scores;
+		this.getScores();
+	}
+
+	getScores() {
+		fetch("http://data.cs.purdue.edu:5001/getScores", {
+			method: "POST",
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"	
+			},
+			body: JSON.stringify({	
+				game: this.game
+			})
+		}).then(function(response) {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error("Network response was not ok");
+		}).then(function(data) {
+				this.scores = data.scores;
+		}.bind(this));
+	}
+}
