4fb37396cc7b58b003dc8c2751b8561618e5636b
Ryan Sullivan <ryanpnavillus@gmail.com>
2017-12-07 14:57:57 -0500

    callback

--- LoadScoreBoard.js
+++ LoadScoreBoard.js
@@ -1,5 +1,10 @@
+
+
 $( document ).ready( function() {
-	var blackjackBoard = new ScoreBoard("BlackJack");
+	var blackjackBoard = new ScoreBoard("BlackJack", function() {
+		document.getElementById("score");
+
+	});
 	var mineBoard = new ScoreBoard("Mine");
 	var numberBoard = new ScoreBoard("TestGameAI");
 
